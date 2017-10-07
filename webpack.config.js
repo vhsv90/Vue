1a978fb44643637a264eaaa2738a10ac1317daa5
Victor.SanchezValverde <Victor.SanchezValverde@qpharmacorp.com>
2017-10-06 18:45:59 -0600

    Easy example of Vue.js application

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.vue$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'vue-loader'
+          }
+        }
+      ]
+    }
+  }
